d221e88c76517bf07f23fe33b1f5dd1cdff545bf
Prekshith Reddy <prekshithreddy2728@gmail.com>
2026-08-18 05:53:19 +0530

    Time: 35 ms (43.11%), Space: 58.9 MB (86.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0;i<n-2;i++)
+        for(int i=0;i<nums.length-2;i++)
         {
-            int j=i+1;
-            int k=n-1;
-            if(i>0 && nums[i]==nums[i-1])
-            
-                continue;
-            
-        while(j<k)
-        {
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum==0)
+            if(i>0&&nums[i]==nums[i-1])
             {
-                res.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                j++;
-                k--;
-                while(j<k && nums[j]==nums[j-1])
-                j++;
+                continue;
             }
-            else if(sum<0)
-            
-                j++;
-            
-            else
-                k--;
-            
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right)
+            {
+                int sum=nums[i]+nums[right]+nums[left];
+                if(sum==0)
+                {
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left]==nums[left+1])
+                    {
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right-1])
+                    {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+                else if(sum<0)
+                {
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
         }
-        }
-        return res;
+        return ans;
     }
 }
