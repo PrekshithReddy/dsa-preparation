20b426e1078a3755ce07562547c5ea01eb55a6ae
Prekshith Reddy <prekshithreddy2728@gmail.com>
2026-09-04 06:47:07 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (49.36%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        // Suffix minimum array
+        int[] right = new int[n];
+
+        right[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.min(right[i + 1], nums[i]);
+        }
+
+        // Find the smallest stable index
+        int leftMax = 0;
+
+        for (int i = 0; i < n; i++) {
+            leftMax = Math.max(leftMax, nums[i]);
+
+            if (leftMax - right[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
